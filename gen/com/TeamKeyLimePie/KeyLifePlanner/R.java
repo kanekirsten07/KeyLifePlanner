3d492a5b144bed066d47cf09ec8a3a2cd7a1cf04
Kirsten Kane <kanekirsten07@gmail.com>
2013-04-14 23:07:45 -0700

    Avatar Activity
    Added avatar view and changed its background color

--- gen/com/TeamKeyLimePie/KeyLifePlanner/R.java
+++ gen/com/TeamKeyLimePie/KeyLifePlanner/R.java
@@ -40,14 +40,16 @@ public final class R {
         public static final int viewAvatar=0x7f0a0003;
     }
     public static final class layout {
-        public static final int activity_calendar=0x7f030000;
-        public static final int activity_calendar_view=0x7f030001;
-        public static final int activity_key_life_planner__main=0x7f030002;
+        public static final int activity_avatar=0x7f030000;
+        public static final int activity_calendar=0x7f030001;
+        public static final int activity_calendar_view=0x7f030002;
+        public static final int activity_key_life_planner__main=0x7f030003;
     }
     public static final class menu {
-        public static final int calendar=0x7f090000;
-        public static final int calendar_view=0x7f090001;
-        public static final int key_life_planner__main=0x7f090002;
+        public static final int avatar=0x7f090000;
+        public static final int calendar=0x7f090001;
+        public static final int calendar_view=0x7f090002;
+        public static final int key_life_planner__main=0x7f090003;
     }
     public static final class raw {
         public static final int beginnings=0x7f040000;
@@ -56,12 +58,14 @@ public final class R {
     public static final class string {
         public static final int AchievementsButtonLabel=0x7f06000a;
         public static final int AddButtonLabel=0x7f060009;
+        public static final int Avatar=0x7f06000b;
         public static final int AvatarButtonLabel=0x7f060008;
         public static final int CalendarButtonLabel=0x7f060007;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int calendar=0x7f060006;
         public static final int hello_world=0x7f060004;
+        public static final int title_activity_avatar=0x7f06000c;
         public static final int title_activity_calendar=0x7f060003;
         public static final int title_activity_calendar_view=0x7f060005;
         public static final int welcome=0x7f060002;
