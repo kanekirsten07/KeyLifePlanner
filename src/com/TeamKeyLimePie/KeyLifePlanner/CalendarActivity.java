a0640f46c28725bf495050c35fcc11fe53c4a0b5
Kirsten Kane <kanekirsten07@gmail.com>
2013-04-15 12:22:14 -0700

    EditText Field
    Added layout and implementation for an editable text field

--- src/com/TeamKeyLimePie/KeyLifePlanner/CalendarActivity.java
+++ src/com/TeamKeyLimePie/KeyLifePlanner/CalendarActivity.java
@@ -8,12 +8,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class CalendarActivity extends Activity implements OnClickListener{
 
@@ -25,17 +30,32 @@ public class CalendarActivity extends Activity implements OnClickListener{
 	private ArrayList<Task> Thursday;
 	private ArrayList<Task> Friday;
 	private ArrayList<Task> Saturday;
+	EditText taskedit;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calendar_view);
 		
-Button addTask = (Button)findViewById(R.id.addtask);
+		Button addTask = (Button)findViewById(R.id.addtask);
 		
 		addTask.setOnClickListener(this);
-       
-     
+       taskedit = (EditText)findViewById(R.id.taskname);
+    
+       taskedit.setOnFocusChangeListener(new OnFocusChangeListener() {
+    	   public void onFocusChange(View v, boolean hasFocus){
+    		   String strvalue = taskedit.getText().toString();
+    		   Log.d("EditText value:", strvalue);
+    	   }
+       });
 	}
+	
+	
+	
+	
+	
+	
+	
+	
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
