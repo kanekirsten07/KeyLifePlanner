11878d1e46debd285a8e2b3870314ca81fa2811c
Kirsten Kane <kkane@brand-up.com>
2013-04-14 22:40:47 -0700

    Revert "Revert "Added the third button, background music, and color""
    This reverts commit a9863c37ab4e8a85dae278155eb8382a97e31e49.
    
    Trying to revert this commit to make the repo not corrupted

--- gen/com/TeamKeyLimePie/KeyLifePlanner/R.java
+++ gen/com/TeamKeyLimePie/KeyLifePlanner/R.java
@@ -10,6 +10,13 @@ package com.TeamKeyLimePie.KeyLifePlanner;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int AchievementsButtonColor=0x7f070002;
+        public static final int AddTaskButtonColor=0x7f070004;
+        public static final int AvatarButtonColor=0x7f070003;
+        public static final int BackgroundColor=0x7f070000;
+        public static final int CalendarButtonColor=0x7f070001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,17 +24,20 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int calendarActivity=0x7f080001;
-        public static final int calendarView=0x7f080000;
-        public static final int viewAvatar=0x7f080002;
+        public static final int action_settings=0x7f0a0006;
+        public static final int button1=0x7f0a0001;
+        public static final int calendarActivity=0x7f0a0005;
+        public static final int calendarView=0x7f0a0000;
+        public static final int textView1=0x7f0a0002;
+        public static final int viewAchievements=0x7f0a0004;
+        public static final int viewAvatar=0x7f0a0003;
     }
     public static final class layout {
         public static final int activity_calendar=0x7f030000;
@@ -35,21 +45,26 @@ public final class R {
         public static final int activity_key_life_planner__main=0x7f030002;
     }
     public static final class menu {
-        public static final int calendar=0x7f070000;
-        public static final int calendar_view=0x7f070001;
-        public static final int key_life_planner__main=0x7f070002;
+        public static final int calendar=0x7f090000;
+        public static final int calendar_view=0x7f090001;
+        public static final int key_life_planner__main=0x7f090002;
+    }
+    public static final class raw {
+        public static final int beginnings=0x7f040000;
+        public static final int success=0x7f040001;
     }
     public static final class string {
-        public static final int AddButtonLabel=0x7f050009;
-        public static final int AvatarButtonLabel=0x7f050008;
-        public static final int CalendarButtonLabel=0x7f050007;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int calendar=0x7f050006;
-        public static final int hello_world=0x7f050004;
-        public static final int title_activity_calendar=0x7f050003;
-        public static final int title_activity_calendar_view=0x7f050005;
-        public static final int welcome=0x7f050002;
+        public static final int AchievementsButtonLabel=0x7f06000a;
+        public static final int AddButtonLabel=0x7f060009;
+        public static final int AvatarButtonLabel=0x7f060008;
+        public static final int CalendarButtonLabel=0x7f060007;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int calendar=0x7f060006;
+        public static final int hello_world=0x7f060004;
+        public static final int title_activity_calendar=0x7f060003;
+        public static final int title_activity_calendar_view=0x7f060005;
+        public static final int welcome=0x7f060002;
     }
     public static final class style {
         /** 
@@ -73,10 +88,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
     }
 }
