d84e26df6fa37f3f7fe330d0332ee23d3bd17d83
Alex <atutt113@gmail.com>
2013-04-17 15:36:53 -0700

    Hard code of achievements

--- src/com/TeamKeyLimePie/KeyLifePlanner/AchievementActivity.java
+++ src/com/TeamKeyLimePie/KeyLifePlanner/AchievementActivity.java
@@ -27,11 +27,52 @@ public class AchievementActivity extends Activity {
 		
 		ArrayList<Achievement> ach = new ArrayList<Achievement>();
 		
-		Achievement a1 = new Achievement("use this app", false);
-		Achievement a2 = new Achievement("Complete two hygiene tasks in one day", false);
+		Achievement a1 = new Achievement("First Day!" + "\n"+ " Welcome to Key Life Planner!", false);
+		Achievement a2 = new Achievement("Clean!" + "\n"+" You Have Completed a Hygiene Tasks", false);
+		Achievement a3 = new Achievement("Hard Working!" + "\n"+" You have completed a Work task", false);
+		Achievement a4 = new Achievement("Good Student!" + "\n"+" You have compeleted a School task", false);
+		Achievement a5 = new Achievement("Clean as a Whistle!" + "\n"+" You Have Completed all Hygiene Tasks For One Day", false);
+		Achievement a6 = new Achievement("Far Voyager!"+ "\n"+"You Have Completed a Travel Task", false);
+		Achievement a7 = new Achievement("Well Rounded!" + "\n"+" You Have Completed a Hygiene Task, a Work Task and a School Task", false);
+		Achievement a8 = new Achievement("Just Getting Started!" + "\n"+" You Have Completed All Your Tasks For One Day", false);
+		Achievement a9 = new Achievement("Keep it Up!" + "\n"+" You Have Completed All Your Tasks For Two Days ", false);
+		Achievement a10 = new Achievement("Doing Great!" + "\n"+" You Have Completed All Your Tasks For 30 Days", false);
+		Achievement a11 = new Achievement("Epic!" + "\n"+" You Have Completed All Your Tasks for 365 Days",false);
+		Achievement a12 = new Achievement("Customize!" + "\n"+" You Have Changed Your Avatar", false);
+		Achievement a13 = new Achievement("Making Money!" + "\n"+" You Have Earned Your First Coin", false);
+		Achievement a14 = new Achievement("Cashing In!" + "\n"+" You Have Earned 25 Coins", false);
+		Achievement a15 = new Achievement("Centenial Cash!"+ "\n"+"You Have Earned 100 Coins", false);
+		Achievement a16 = new Achievement("Shopper!"+ "\n"+"You Have Bought an Item From the Store", false);
+		Achievement a17 = new Achievement("Task Apprentice!"+ "\n"+"You Have Completed 8 Tasks", false);
+		Achievement a18 = new Achievement("Task Journeyman!"+ "\n"+"You Have Completed 64 Tasks", false);
+		Achievement a19 = new Achievement("Task Master!"+ "\n"+"You Have Completed 128 Tasks", false);
+		Achievement a20 = new Achievement("Task Sage!"+ "\n"+"You Have Completed 512 Tasks", false);
+
+		
+
 		
 		ach.add(a1);
 		ach.add(a2);
+		ach.add(a3);
+		ach.add(a4);
+		ach.add(a5);
+		ach.add(a6);
+		ach.add(a7);
+		ach.add(a8);
+		ach.add(a9);
+		ach.add(a10);
+		ach.add(a11);
+		ach.add(a12);
+		ach.add(a13);
+		ach.add(a14); 
+		ach.add(a15);
+		ach.add(a16);
+		ach.add(a17);
+		ach.add(a18);
+		ach.add(a19);
+		ach.add(a20); 
+		
+		
 		achieve_view = (ListView)findViewById(R.id.achievementlist);
 		achieve_view.setAdapter(new UserItemAdapter(this, android.R.layout.simple_list_item_1, ach));
 	}
